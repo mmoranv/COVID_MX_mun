d60530bfcb924a98d287a02eb7bc79da4ce11aac
Mariano <mmoranve@gmail.com>
2020-07-05 12:57:54 -0400

    Started mapping
    Started mapping

--- R/covid_MX_mun.R
+++ R/covid_MX_mun.R
@@ -16,6 +16,11 @@ library(lubridate)
 library(gganimate)
 library(gifski)
 library(reshape2)
+require("maptools")
+require("plyr")
+library(rgdal)
+library(geojsonio)
+library(leaflet)
 
 setwd("C:/Users/Mariano/Documents/GitHub/COVID_MX_mun/")
 
@@ -46,54 +51,61 @@ casos_covid_municipio <- "DATA/Casos_Diarios_Municipio_Confirmados_20200628.csv"
   mutate(casos_acum = cumsum(casos_nuevos)) %>% #cumsum, suma acumulada
   #mutate(casos_acum_100 = (casos_acum/pop)*100000)) %>%   #ajustada por poblacion
   mutate(casos_acum_100 = 100000 * (cumsum(casos_nuevos)/pop)) %>%
-  ungroup() 
+  ungroup()
 
 casos_covid_municipio[4:64] <-NULL  #No necesito las columnas por cada fecha ya, las tengo como filas
+casos_covid_municipio_1 <- casos_covid_municipio[1:3,65:68]
 #Asi, solo necesitas seleccionar que fecha quieres para graficar todos los casos de todos los municipios de cierta fecha
 
 #Ahora a intentarlo por melt y cast #CREO QUE NO SE PUEDE
 
-casos_covid_municipio_1 <- "DATA/Casos_Diarios_Municipio_Confirmados_20200628.csv" %>% 
-  read_csv() %>% 
-  rename( #cambiar nombres a columnas para claridad y comodidad
-    id = cve_ent,
-    pop = poblacion,
-    nombre = nombre
-  )
-
-fechas <- colnames(casos_covid_municipio_1)
-fechas_2 <- fechas[4:172]
-
-casos_covid_municipio_2 <-  melt(casos_covid_municipio_1, id=c("id"))
-casos_covid_municipio_2 <-  melt(casos_covid_municipio_1, id=c(fechas_2,"id"))
-
-#(mydata, id=c("id_variable_1","id_variable_2")) #you're creating a vector of identifiers, whatever should categorize but NOT be added
-casos_cast <- dcast(casos_covid_municipio_2, id~c(fechas_2), cumsum)
-
 ####################################################################################
 ####################################################################################
-##########################    2.GRAPH?/ MAP?   ####################################
+##########################    2.GRAPH?/ MAP?    ####################################
 ####################################################################################
 ####################################################################################
 ####################################################################################
 
-p <- casos_covid_municipio %>% 
-  # 10 random cities
-  filter(id %in% sample(unique(id), 10)) %>% 
-  filter(fecha == "28-07-2020") %>% 
-  ggplot() +
-  aes(x = nombre, y = casos_acum) +
-  coord_flip() +
-  geom_col() +
-  transition_time(fecha) +
-  ease_aes('cubic-in-out') +
-  exit_fade() +
-  labs(
-    title = 'Casos Confirmados Acumulados',
-    subtitle = '{frame_time}'
-  )
+#First I'll try to map one cumulative number of cases in one date
+#Import geojson municipal boundary map (copied the console response from https://datos.covid-19.conacyt.mx/#COMNac)
 
-p
+mexico_municipios = readOGR(dsn="C:/Users/Mariano/Documents/ArcGIS/MEX/COVID_19_SSA_Mex/ssa_covid_template.geojson")
+mexico_municipios <- geojson_read("C:/Users/Mariano/Documents/ArcGIS/MEX/COVID_19_SSA_Mex/ssa_covid_template.geojson",what="sp")
+
+#Import modified shapefile from INEGI, then leaflet
+mexico_municipios_INEGI <- readOGR( 
+  dsn= "C:/Users/Mariano/Documents/ArcGIS/MEX/Marco geoestadistico/municipios.shp", 
+  layer="municipios",
+  verbose=FALSE
+)
+
+# Clean the data object #aqui podria modificar los datos... pero no necesito hacerlo
+#library(dplyr)
+#world_spdf@data$POP2005[ which(world_spdf@data$POP2005 == 0)] = NA
+#world_spdf@data$POP2005 <- as.numeric(as.character(world_spdf@data$POP2005)) / 1000000 %>% round(2)
+
+
+# Plot mexico, try
+mypalette <- colorNumeric( palette="viridis", domain=world_spdf@data$POP2005, na.color="transparent")
+mypalette(c(45,43))
+mexico <- leaflet(mexico_municipios_INEGI) #%>% 
+  addTiles() %>% 
+  addPolygons()
+
+mexico
+
+?readOGR
+
+#Join to data
+
+#Then iteratively
+
+####################################################################################
+####################################################################################
+##########################    3.ANIMATE       ######################################
+####################################################################################
+####################################################################################
+####################################################################################
 
 animate(p, duration = 5, fps = 20, width = 600, height = 600, renderer = gifski_renderer())
 
@@ -101,11 +113,9 @@ anim_save("covid-cases-plot.gif")
 
 ####################################################################################
 ####################################################################################
-##########################    3.EXPORT     ####################################
+##########################    4.EXPORT     ####################################
 ####################################################################################
 ####################################################################################
 ####################################################################################
 
-casos_covid_municipio %>% 
-  write_csv("C:/Users/Mariano/Documents/ArcGIS/MEX/COVID_19_SSA_Mex/DATA/Acumulados_al_20200628.csv")
-  
+write.csv(casos_covid_municipio, "C:/Users/Mariano/Documents/ArcGIS/MEX/COVID_19_SSA_Mex/DATA/casos_covid_municipio.csv")
