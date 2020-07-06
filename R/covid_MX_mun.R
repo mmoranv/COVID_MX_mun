2d996237c5e60745a6ff1090067055fe3a635939
Mariano <mmoranve@gmail.com>
2020-07-05 23:46:19 -0400

    Mapping and joining data

--- R/covid_MX_mun.R
+++ R/covid_MX_mun.R
@@ -10,7 +10,6 @@
 
 #Exportados:
 #C:\Users\Mariano\Documents\ArcGIS\MEX\COVID_19_SSA_Mex\EXPORTED
-install.packages("tidyverse")
 library(tidyverse)
 library(lubridate)
 library(gganimate)
@@ -38,7 +37,7 @@ casos_covid_municipio <- "DATA/Casos_Diarios_Municipio_Confirmados_20200628.csv"
   rename( #cambiar nombres a columnas para claridad y comodidad
     id = cve_ent,
     pop = poblacion,
-    nombre = nombre
+    mun = nombre
   ) %>% 
   pivot_longer(
     cols = matches("\\d{2}-\\d{2}-\\d{4}"), #los pivotes seran solo columnas que sean fechas
@@ -50,11 +49,13 @@ casos_covid_municipio <- "DATA/Casos_Diarios_Municipio_Confirmados_20200628.csv"
   arrange(id, fecha) %>% 
   group_by(id) %>% #agrupar por municipio
   mutate(casos_acum = cumsum(casos_nuevos)) %>% #cumsum, suma acumulada
-  #mutate(casos_acum_100 = (casos_acum/pop)*100000)) %>%   #ajustada por poblacion
-  mutate(casos_acum_100 = 100000 * (cumsum(casos_nuevos)/pop)) %>%
+  mutate(casos_acum_ajustados = 100000 * (cumsum(casos_nuevos)/pop)) %>% #ajustada por poblacion
   ungroup()
 
+casos_covid_municipio$casos_acum_ajustados <- format(casos_covid_municipio$casos_acum_ajustados, digits=2, nsmall=2) #acortar el numero de casos dividido por poblacion
 casos_covid_municipio[4:64] <-NULL  #No necesito las columnas por cada fecha ya, las tengo como filas
+casos_covid_municipio$casos_acum_ajustados <- as.numeric(casos_covid_municipio$casos_acum_ajustados)
+casos_covid_municipio
 
 #Asi, solo necesitas seleccionar que fecha quieres para graficar todos los casos de todos los municipios de cierta fecha
 
@@ -77,17 +78,55 @@ municipios_sf <- st_read(dsn = "C:/Users/Mariano/Documents/ArcGIS/MEX/Marco geoe
   st_transform(4326)
 
 
+#######Join data to map
+mexico <- municipios_sf %>%
+  mutate(id = cve_num) %>%
+  full_join(casos_covid_municipio, by = "id")
+#Wait! How do I join it! I will have many rows (dates) per every municipality. What I need for mapping is only one row per municipality
+#maybe i have to join it by date?
+#or i could filter it by day and then map?
+
+#14052020
+mexico_14052020 <- mexico %>%
+  filter(fecha == "14-05-2020")
+
+#Map
 ggplot(data = municipios_sf) +
   geom_sf()
 
-#Join data to map
+#It works! Let's build the map now!
+map_14052020 <- ggplot() +
+  geom_sf(data = mexico_14052020, color = "white", size = .2) +
+  aes(fill = casos_acum_ajustados) +
+  scale_fill_gradient(low = "white", high = "red", na.value = "gray", guide = "legend")
+
+
+map_14052020
+
+#14062020
+mexico_14062020 <- mexico %>%
+  filter(fecha == "14-06-2020")
 
 #It works! Let's build the map now!
-ggplot() +
-  geom_sf(data = mexico, color = "red", size = .3) +
-  scale_fill_manual(values = c("#cbc9e2", "#9e9ac8", "#756bb1", "#54278f"),
-                    labels = c("$2,000 or less", "$2,001-$2,500", "$2,501-$3,000", 
-                               "More than $3,000")) +
+map_14062020 <- ggplot() +
+  geom_sf(data = mexico_14062020, color = "white", size = .2) +
+  aes(fill = casos_acum_ajustados) +
+  scale_fill_gradient(low = "white", high = "red", na.value = "gray", guide = "legend")
+
+map_14062020
+
+
+?scale_fill_gradient()
+
+?palette
+palette
+?colorRamp
+?scale_fill_continuous
+
+
+
+(values = c("#cbc9e2", "#9e9ac8", "#756bb1", "#54278f"),
+                    labels = c(10,20,30,100)) #+
   coord_sf(datum = NA) + #removes the grid
   theme_void() + #removes the default theme
   theme(
@@ -103,16 +142,19 @@ ggplot() +
     fill = "", #no legend title
     title = "Figure 5: Median Asking Rent by Community District, 2018")#,
     #caption = sources)
-library(sf)
-nc <- st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
-library(ggplot2)
-ggplot(nc) + geom_sf(aes(fill = AREA))
-mex <- ggplot(mexico) + aes(fill = cve_num)
-sessionInfo()
 
-#Join to data
+#Now iteratively
 
-#Then iteratively
+  #trying
+  p <- mexico %>% 
+  # 10 random dates
+  filter(fecha %in% sample(unique(fecha), 10)) %>%
+  arrange(fecha) +
+  ggplot() +
+  geom_sf(data = mexico, color = "white", size = .2) +
+  aes(fill = casos_acum_ajustados) +
+  scale_fill_gradient(low = "white", high = "red", na.value = "gray", guide = "legend") +
+  transition_time(fecha)
 
 ####################################################################################
 ####################################################################################
@@ -120,8 +162,11 @@ sessionInfo()
 ####################################################################################
 ####################################################################################
 ####################################################################################
+?animate
 
-animate(p, duration = 5, fps = 20, width = 600, height = 600, renderer = gifski_renderer())
+m <- map_14042020  
+    
+animate(, duration = 5, fps = 20, width = 600, height = 600, renderer = gifski_renderer())
 
 anim_save("covid-cases-plot.gif")
 
